ea374a5fbdc0a4fad523eeb888c6324d171a7672
yornipaz <pazfelipe1996@hotmail.com>
2021-12-03 16:06:40 -0500

    Configuracion inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PlaceComponent } from './pages/place/place.component';
+import { CharacterComponent } from './components/character/character.component';
+import { SearchComponent } from './components/search/search.component';
+import { HouseComponent } from './components/house/house.component';
+import { FilterCharacterComponent } from './components/filter-character/filter-character.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PlaceComponent,
+    CharacterComponent,
+    SearchComponent,
+    HouseComponent,
+    FilterCharacterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/filter-character/filter-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-character',
+  templateUrl: './filter-character.component.html',
+  styleUrls: ['./filter-character.component.scss']
+})
+export class FilterCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
